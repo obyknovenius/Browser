349bb05e3633a5caa55e546e6c6063474ffd937b
Vitaly Dyachkov <obyknovenius@me.com>
2021-12-21 18:28:01 +0100

    Initial Commit

---
+++ Browser/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Browser
+//
+//  Created by Vitaly Dyachkov on 21.12.21.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Browser/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Browser
+//
+//  Created by Vitaly Dyachkov on 21.12.21.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
