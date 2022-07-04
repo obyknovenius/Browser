3d4ca9fddb4461d09e85edde6c30699fdf80da10
Vitaly Dyachkov <obyknovenius@me.com>
2022-07-04 20:21:56 +0200

    Draw a line of text in the View

--- Browser/View.mm
+++ Browser/View.mm
@@ -9,8 +9,43 @@
 
 @implementation View
 
-- (void)drawRect:(NSRect)dirtyRect {
-    CGContextRef context = [[NSGraphicsContext currentContext] CGContext];
+- (BOOL)isFlipped
+{
+    return YES;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    CGContextRef context = [NSGraphicsContext currentContext].CGContext;
+    
+    CGContextSetTextMatrix(context, CGAffineTransformMakeScale(1.0f, -1.0f));
+    
+    CFStringRef string = CFSTR("Hello World!");
+    
+    CFStringRef fontName = CFSTR("Helvetica");
+    CGFloat fontSize = 24.0f;
+    CTFontRef font = CTFontCreateWithName(fontName, fontSize, NULL);
+    CFRelease(fontName);
+    
+    CFStringRef keys[] = { kCTFontAttributeName };
+    CFTypeRef values[] = { font };
+    
+    CFDictionaryRef attributes = CFDictionaryCreate(NULL, (const void**)&keys,
+                                                    (const void**)&values, sizeof(keys) / sizeof(keys[0]),
+                                                    &kCFTypeDictionaryKeyCallBacks,
+                                                    &kCFTypeDictionaryValueCallBacks);
+    CFRelease(font);
+    
+    CFAttributedStringRef attrString = CFAttributedStringCreate(NULL, string, attributes);
+    CFRelease(string);
+    CFRelease(attributes);
+    
+    CTLineRef line = CTLineCreateWithAttributedString(attrString);
+    CFRelease(attrString);
+    
+    CGContextSetTextPosition(context, 0, CTFontGetAscent(font));
+    CTLineDraw(line, context);
+    CFRelease(line);
 }
 
 @end
