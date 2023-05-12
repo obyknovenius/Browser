1a44c26c434e446c5c8910dfcced4ae08b4aec57
Vitaly Dyachkov <obyknovenius@me.com>
2023-05-12 10:36:30 +0200

    Add Flatpak manifest

--- Source/Browser/main.cpp
+++ Source/Browser/main.cpp
@@ -4,6 +4,6 @@
 
 int main(int argc, char** argv)
 {
-    auto app = Gtk::Application::create("com.obyknovenius.Browser");
+    auto app = Gtk::Application::create("com.github.obyknovenius.Browser");
     return app->make_window_and_run<Browser::Window>(argc, argv);
 }
